627657a115b3dda583d7302d23fc985bf0c2147f
jsq5m <jaumesala@inspladelestany.cat>
2021-10-24 19:51:55 +0200

    Initial commit

---
+++ app/src/main/java/cat/dam/jaume/gridlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.dam.jaume.gridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
